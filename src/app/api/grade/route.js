ada07697437f150ddc876a4207c1d261488e17b2
harzjunior <harunjibs@gmail.com>
2024-05-23 02:46:10 +0100

    fetch the grade api from db

--- src/app/api/grade/route.js
+++ src/app/api/grade/route.js
@@ -1,5 +1,10 @@
 const { NextResponse } = require("next/server");
 
+import { db } from "@/utils";
+import { GRADES } from "@/utils/schema";
+
 export async function GET(req) {
-  return NextResponse.json({ test: "Hello Api" });
+  // get the grades from the table
+  const result = await db.select().from(GRADES);
+  return NextResponse.json(result);
 }
