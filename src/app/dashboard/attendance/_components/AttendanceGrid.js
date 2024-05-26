16591052281fcf0270c369439ab90a964bd6a80d
harzjunior <harunjibs@gmail.com>
2024-05-26 02:08:23 +0100

    displayed the searched attendance record top the page

--- src/app/dashboard/attendance/_components/AttendanceGrid.js
+++ src/app/dashboard/attendance/_components/AttendanceGrid.js
@@ -1,18 +1,24 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 
 //got attendanceListData props from Attendance component
 function AttendanceGrid({ attendanceListData }) {
   // Row ag-grid Data: The data to be displayed.
   const [rowData, setRowData] = useState([]);
+  const [colDefs, setColDefs] = useState([
+    { field: "studentId" },
+    { field: "fullName" },
+  ]);
 
   // only refresh the page when there is attendanceListData data
   useEffect(() => {
     // Ensure attendanceListData is valid, the API response indicates success, and result is an array
     if (attendanceListData?.result) {
       const uniqueRecords = getUniqueRecords(attendanceListData.result);
-      console.log(uniqueRecords);
       setRowData(uniqueRecords);
     }
   }, [attendanceListData]);
@@ -32,7 +38,14 @@ function AttendanceGrid({ attendanceListData }) {
     return uniqueRecord;
   };
 
-  return <div>Attendance</div>;
+  return (
+    <div
+      className="ag-theme-quartz" // applying the grid theme
+      style={{ height: 500 }} // the grid will fill the size of the parent container
+    >
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    </div>
+  );
 }
 
 export default AttendanceGrid;
