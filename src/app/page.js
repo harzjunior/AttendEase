9f9a5b3f5d38b5823da33c0fe57f2ab5cea03543
harzjunior <harunjibs@gmail.com>
2024-05-29 17:31:26 +0100

    updated redirect function to useRouter client-side navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
-"use client";
-
 import { redirect } from "next/navigation";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/api/auth/login?post_login_redirect_url=/dashboard",
+      permanent: false,
+    },
+  };
+}
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/api/auth/login?post_login_redirect_url=/dashboard");
-
-  }, [router]);
   return <main className=""></main>;
 }
