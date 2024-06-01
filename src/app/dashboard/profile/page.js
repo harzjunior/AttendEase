a1fe45172ed0cc3739ccedc513c3408a7579f91d
harzjunior <harunjibs@gmail.com>
2024-06-02 00:39:48 +0100

    fresh working branch on profile page

--- src/app/dashboard/profile/page.js
+++ src/app/dashboard/profile/page.js
@@ -1,7 +1,11 @@
 import { StudentProfile } from "../students/_components/StudentProfile";
 
 function Profile() {
-  return <div className="">Profile</div>;
+  return (
+    <div className="">
+      <h1>Profile</h1>
+    </div>
+  );
 }
 
 export default Profile;
